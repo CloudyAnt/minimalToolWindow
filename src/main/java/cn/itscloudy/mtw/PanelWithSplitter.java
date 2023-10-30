38a998e58605225e4171cfea323e576f58575de8
cloudy <iiim.cloudy@gmail.com>
2023-10-30 14:44:52 +0800

    Chai refactor: set one line mode true

--- src/main/java/cn/itscloudy/mtw/PanelWithSplitter.java
+++ src/main/java/cn/itscloudy/mtw/PanelWithSplitter.java
@@ -25,10 +25,18 @@ public class PanelWithSplitter {
     private JPanel bottomPanel;
     private JPanel tfHolder;
 
-    String code = "public static void main(String[] args) {\n" +
-            "    int a = 0;\n" +
-            "    int b = 1;\n" +
-            "    System.out.println(a + b);\n" +
+    String code = "public static int compareVersion(String version1, String version2) {\n" +
+            "    String[] nums1 = version1.split(\"\\\\.\");\n" +
+            "    String[] nums2 = version2.split(\"\\\\.\");\n" +
+            "    int n1 = nums1.length, n2 = nums2.length;\n" +
+            "    for (int i = 0; i < Math.max(n1, n2); ++i) {\n" +
+            "        int v1 = i < n1 ? Integer.parseInt(nums1[i]) : 0;\n" +
+            "        int v2 = i < n2 ? Integer.parseInt(nums2[i]) : 0;\n" +
+            "        if (v1 != v2) {\n" +
+            "            return v1 > v2 ? 1 : -1;\n" +
+            "        }\n" +
+            "    }\n" +
+            "    return 0;\n" +
             "}";
 
     public PanelWithSplitter(Project project) {
@@ -71,6 +79,7 @@ public class PanelWithSplitter {
                             editorEx.setBackgroundColor(c != null ? c : scheme.getDefaultBackground());
                             editorEx.setColorsScheme(scheme);
                         })));
+        tf.setOneLineMode(true);
         ApplicationManager.getApplication().invokeLater(() -> tf.setText(code));
         return tf;
     }
